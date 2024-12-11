79e204d969daa2a827ab3189c4e266cde9ad08ab
TLiLi <tlilios@yahoo.com>
2024-12-11 04:43:35 +0100

    RedoTheprojectP6

---
+++ vitesse2/vitesse2/vitesse2App.swift
@@ -0,0 +1,17 @@
+//
+//  vitesse2App.swift
+//  vitesse2
+//
+//  Created by TLiLi Hamdi on 11/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct vitesse2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
